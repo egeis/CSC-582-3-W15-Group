673526703aa932663eafe090a2b247239b46a989
egeis <rcoan@callutheran.edu>
2014-12-27 18:17:08 -0800

    Removed Older Merge Sort, Updated Merge to handle a single element.

--- src/sort/Merge.java
+++ src/sort/Merge.java
@@ -21,43 +21,56 @@ public class Merge<T extends Comparable<T>> implements Callable<T[]> {
     private T[] b;
     private FutureTask<T[]> fa;
     private FutureTask<T[]> fb;
+    private List<T> results = new ArrayList<T>(); 
+    private boolean isDone = false;
     
-    public Merge(FutureTask<T[]> fa, FutureTask<T[]>fb) {
+    public Merge(T values) {
+        results.add(values);
+        isDone = true;
+    }
+    
+    public Merge(FutureTask<T[]> fa, FutureTask<T[]> fb) {
         this.fa = fa;
         this.fb = fb;
     }
     
     public T[] call() throws Exception {        
         int i = 0, j = 0, k = 0;
-        
-        T[] a = fa.get();
-        T[] b = fb.get();
-        
-        List<T> results = new ArrayList<T>(); 
+                 
+        if(!isDone) {
+            try {
+                a = fa.get();
+                b = fb.get();
+            } catch(Exception e) {
+                System.out.println("[Error]");
+            }
+            
+            while(i < a.length && j < b.length) {
+                if(b[j].compareTo(a[i]) < 0) {
+                    results.add(b[j]);
+                    j++;
+                } else {
+                    results.add(a[i]);
+                    i++;
+                }
 
-        while(i < a.length && j < b.length) {
-            if(b[j].compareTo(a[i]) < 0) {
-                results.add(b[j]);
-                j++;
-            } else {
+                k++;
+            }
+
+            while(i < a.length) {
                 results.add(a[i]);
+                k++;
                 i++;
             }
 
-            k++;
+            while(j < b.length) {
+                results.add(b[j]);
+                k++;
+                j++;
+            }
         }
-
-        while(i < a.length) {
-            results.add(a[i]);
-            k++;
-            i++;
-        }
-
-        while(j < b.length) {
-            results.add(b[j]);
-            k++;
-            j++;
-        }
+        
+        System.out.println("[Debug]: Merge Length "+results.size());
         
         return results.toArray( (T[]) Array.newInstance(results.getClass(), 0) );
     }
--- src/sort/ParallelMerge.java
+++ src/sort/ParallelMerge.java
@@ -7,12 +7,13 @@ package sort;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import static sort.ParallelMergeSort.executor;
+import java.util.concurrent.FutureTask;
 
 /**
  *
@@ -21,29 +22,63 @@ import static sort.ParallelMergeSort.executor;
 public class ParallelMerge<T extends Comparable<T>> implements Callable<T[]> {
 
     protected T[] values;
-    protected List<Future<T[]>> list = new ArrayList<Future<T[]>>();
+    protected List<FutureTask<T[]>> list = new ArrayList<FutureTask<T[]>>();
     public static ExecutorService executor;
     
     public ParallelMerge(T[] values) {
         this.values = values;
         if(executor == null) executor = Executors.newCachedThreadPool();
+        
+        for(int i = 0; i < values.length; i++) {
+            FutureTask<T[]> ft = new FutureTask(new Merge(values[i]));
+            list.add(ft);
+            executor.execute(ft);
+        }
     }
     
     public T[] call() throws Exception {
-    
+        System.out.println("[DEBUG] Starting with " + list.size() + " items.");
+        while(list.size() > 1) {
+            FutureTask<T[]> fa = list.remove(0);
+            FutureTask<T[]> fb = list.remove(0);
+
+
+            FutureTask<T[]> ft = new FutureTask(new Merge(fa,fb));
+            list.add(ft);
+            executor.execute(ft);
+            System.out.println("[DEBUG] Looping with " + list.size() + " items.");
+        }
+        
+        FutureTask<T[]> ft = list.remove(0);
+        return ft.get();
     }
     
-    class Single<T extends Comparable<T>> implements Callable<T[]> {
-        private T value;
-
-        public Single(T value) {
-            this.value = value;
+    public static void main(String[] args) {
+        
+        long start, end;
+        Integer[] test = new Integer[4];
+        Random rand = new Random();
+        
+        for(int i = 0; i < test.length; i++) {
+            test[i] = (int) (rand.nextDouble() * 10);
         }
-
-        public T[] call() throws Exception {
-            List<T> results = new ArrayList<T>();
-            results.add(value);
-            return results.toArray( (T[]) Array.newInstance(results.getClass(), 0) );
+        
+        System.out.println(Arrays.toString(test));
+        
+        FutureTask<Integer[]> fa = new FutureTask<Integer[]>(new ParallelMerge(test));
+        start = System.currentTimeMillis();
+        executor.execute(fa);
+        
+        try {
+            test = fa.get();
+        } catch(Exception e) {
+            
         }
+        executor.shutdown();
+        
+        end = System.currentTimeMillis();
+        System.out.println("[DEBUG] The algorithm took: " + (end - start) + " ms");
+        System.out.println(Arrays.toString(test));
+        
     }
 }
