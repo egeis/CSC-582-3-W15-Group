6349501c0523194a855aa378ef4f087c1eb4c4e5
egeis <rcoan@callutheran.edu>
2015-01-23 17:32:13 -0800

    Added some logic for connecting between node and initiator.

--- src/distributed/Initiator.java
+++ src/distributed/Initiator.java
@@ -1,17 +1,154 @@
 package distributed;
 
-import java.lang.IndexOutOfBoundsException;
+import com.sun.glass.ui.Application;
+import distributed.messages.Packet;
+import distributed.messages.Message;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  */
-public class Initiator extends Node {
+public class Initiator {
+    private static Logger logger;
 
-    private Comparable[] arr;
+    private Comparable[] arr;    
+    private boolean completed = false;
     
-    public Initiator(Comparable[] arr)
+    public final static int SERVER_PORT_NUMBER = 8484;
+    private static List<Agent> agents = new ArrayList<Agent>();
+    
+    private ServerSocket server;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+   
+    private Initiator(Comparable[] arr)
     {        
-        super();
         this.arr = arr;
+        logger = Logger.getLogger(Initiator.class.getName());
+    }
+        
+    public void start()
+    {
+        try {
+            server = new ServerSocket(SERVER_PORT_NUMBER);
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        
+        Thread inbound = new Thread() {
+            @Override
+            public void run()
+            {
+                try {
+                    while(true)
+                    {
+                        //Blocks Until a something is sent.
+                        Socket socket = server.accept();   
+                        input = new ObjectInputStream(socket.getInputStream());
+                        Packet p = (Packet) input.readObject();
+
+                        //Do Something with the Packet.
+                        System.out.println(p.toString());
+                        parsePacket(p, socket.getPort());
+
+                        //Close the Socket
+                        input.close();
+                    }
+                } catch (IOException | ClassNotFoundException ex) {
+                    if(server.isClosed())
+                        logger.log(Level.INFO, null, "Connection Terminated");
+                    else
+                        logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+            
+        inbound.start();
+            
+        //Send out the initial data.
+        
+        //Handle the remaining operations.
+        /**
+         * 1) Check the Status of each node if the heartbeat inc. is reached.
+         * 2) Something that can be done for a specific node, IE output something.
+         * 3) At the end of the for-loop do something globally.
+         * 4) repeat if not completed.
+         */
+        while(true && agents.size() > 0) 
+        {
+            for(Agent a : agents)
+            {
+                if(a.active) {
+                    if(System.currentTimeMillis() - a.heartbeat > 1000 )
+                    {
+                        Packet p = new Packet();
+                        p.type = Message.GET_STATE;
+                        sendPacket(p,a);
+
+                        a.heartbeat = System.currentTimeMillis();
+                    }
+
+                    //Do Stuff per node.
+                    //Additional logic on what and how do it is necessary...
+                }
+            }
+
+            //Do something else globally, which is looped every iteration.
+            
+            if(completed) break;
+        }
+            
+        try {    
+            server.close(); //Close the Server, causes Socket to throw Exception.
+            inbound.join(); //Stops the thread.
+        } catch (IOException | InterruptedException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }       
+    }
+    
+    private void sendPacket(Packet p, Agent a)
+    {        
+        try {
+            Socket socket = new Socket("localhost", a.PORT);
+            output = new ObjectOutputStream(socket.getOutputStream());                        
+            output.writeObject(p);
+            a.success = true;
+            a.retry = 0;
+
+        } catch (IOException ex) {
+            logger.log(Level.INFO, "Cannot Connect to PORT:"+a.PORT);
+            a.retry += 1;
+            if(a.success)
+            {
+                if(a.retry > 6) {
+                    logger.log(Level.WARNING, null, ex);
+                    a.active = false;
+                }
+            } else {
+                if(a.retry > 11) {
+                    logger.log(Level.WARNING, null, ex);
+                    a.active = false;
+                }
+            }
+        }
+    }
+    
+    private void parsePacket(Packet p, int port)
+    {
+        int type = p.type;
+        
+        switch(type)
+        {
+           //DO stuff...
+        }
     }
     
     /**
@@ -38,9 +175,30 @@ public class Initiator extends Node {
         int length = 1000;
         int k = 500;
         
+        //Remote Workers
+        agents.add(new Agent(1212));
+        agents.add(new Agent(1213));
+        
         Integer[] sample = sort.output.TestArray.generate(length);
         Initiator node = new Initiator(sample);
+        node.start();
         
         System.out.println("The Kth value (where k = "+k+") is: " + node.getKthValue(length));
     }
+    
+    private static class Agent {
+
+        public Agent(int port) {
+            this.PORT = port;
+            this.heartbeat = System.currentTimeMillis();
+        }
+        
+        public final int PORT;
+        public long heartbeat;
+        public int retry = 0;
+        public boolean success = false;
+        public boolean active = true;
+    }
 }
+
+
--- src/distributed/Node.java
+++ src/distributed/Node.java
@@ -1,17 +1,137 @@
 package distributed;
 
+import distributed.messages.Packet;
+import distributed.messages.Message;
+import distributed.messages.store.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  */
 public class Node {
     
-    public Node()
-    {
+    private static Logger logger;
+    private static int PORT_NUMBER = 1212;
+    private static final int INIT_PORT_NUMBER = Initiator.SERVER_PORT_NUMBER;
+    
+    private boolean shutdown = false;
+    private int status = 1;
         
+    private ServerSocket server;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    
+    private Comparable[] arr;
+    private Comparable pv;
+    
+    private boolean running = false;
+                
+    private static final String[] STATUS_MESSAGE = { 
+        "NODE_NULL",        //0 - Should never be set to 0.
+        "NODE_STARTING",    //1
+        "NODE_WAITING",     //2
+        "NODE_READY",       //3
+        "NODE_BUSY",        //4
+        "NODE_ERROR",       //5
+        "NODE_UNKNOWN",     //6 - Unused
+        "NODE_UNKNOWN",     //7 - Unused
+        "NODE_UNKNOWN",     //8 - Unused
+        "NODE_UNKNOWN",     //9 - Unused
+        "NODE_SHUTDOWN"     //10
+    }; 
+         
+    private Node()
+    {
+        Logger.getLogger(Node.class.getName()+PORT_NUMBER);
+        status = Message.NODE_STARTING;
+        
+        try {
+            server = new ServerSocket(PORT_NUMBER);
+            
+            while(!shutdown)
+            {
+                Socket socket = server.accept();
+                input = new ObjectInputStream(socket.getInputStream());
+                Packet p = (Packet) input.readObject();
+                parsePacket(p);
+            }
+            
+            server.close();
+        } catch (IOException | ClassNotFoundException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private synchronized void sendPacket(Packet p)
+    {        
+        try {
+            Socket out = new Socket("localhost", INIT_PORT_NUMBER);
+            output = new ObjectOutputStream(out.getOutputStream()); 
+            output.writeObject(p);
+            output.flush();
+            out.close();
+        } catch (IOException ex) {
+           logger.log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void parsePacket(Packet p)
+    {
+        int type = p.type;
+        
+        switch(type)
+        {
+            case Message.GET_STATE:
+                Packet r = new Packet();
+                r.type = status;
+                sendPacket(r);
+                break;
+            case Message.SET_INPUT:
+                NodeSetup s = (NodeSetup) p.pack;
+                arr = s.sub;
+                pv = s.pv;
+                status = Message.NODE_READY;
+                break;
+            case Message.SET_GO_LEFT:
+                
+                break;
+            case Message.SET_GO_RIGHT:
+                
+                break;
+            case Message.SET_START: 
+                if(!running)
+                {
+                    running = true;
+                    
+                    //Handle Starting the quick select somehow.
+                    
+                } else {
+                    //Handle Duplicate start attempt???? or not.
+                }
+                break;
+            case Message.SET_TERMINATION:
+                shutdown = true;
+            break;
+                
+            default:
+                logger.log(Level.SEVERE, null, "Unknown Packet: "+type+" "+p.toString());
+        }
     }
     
     public static void main(String[] args)
     {
+        if(args.length > 0)
+        {
+            PORT_NUMBER = Integer.parseInt(args[0]);
+        }
+        
+        logger = Logger.getLogger(Node.class.getName()+PORT_NUMBER);
         Node node = new Node();
     }
 }
