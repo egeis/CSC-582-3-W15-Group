014efd4a2efdad6b0acef441df0c527a8327c243
egeis <rcoan@callutheran.edu>
2015-01-23 17:27:48 -0800

    Checks if pack is null before attempting to use toString().

--- src/distributed/messages/Packet.java
+++ src/distributed/messages/Packet.java
@@ -1,10 +1,18 @@
 package distributed.messages;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Richard Coan
  */
-public class Packet {
+public class Packet implements Serializable {
     public int type = 0;
     public Object pack = null;
+    
+    @Override
+    public String toString()
+    {
+        return "[Packet "+type+" <"+((pack==null)?"null":pack.toString())+">]";
+    }
 }
